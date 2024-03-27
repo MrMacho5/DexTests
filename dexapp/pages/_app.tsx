c597154e34d1fabd6c47aed368bd939dcc7388f6
RealMallu <mallucrypto3@gmail.com>
2024-03-27 20:32:42 +0530

    ch

--- dexapp/pages/_app.tsx
+++ dexapp/pages/_app.tsx
@@ -1,18 +1,14 @@
 import type { AppProps } from "next/app";
+import { Tabi } from "@thirdweb-dev/chains";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import Navbar from "../components/Navbar.tsx";
-
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "Tabi";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
+      activeChain={ Tabi }
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
     >
       <Navbar />
       <Component {...pageProps} />
