5efda4bb09d6f0562f4d89d1b2d7cc93b9b60574
Mallu Crypto <161009845+RealMallu@users.noreply.github.com>
2024-03-30 00:06:02 +0530

    Update _app.tsx

--- dexapp/pages/_app.tsx
+++ dexapp/pages/_app.tsx
@@ -1,13 +1,12 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      chainRpc={{ [9789.Testnet]: " https://rpc.testnet.tabichain.com" }}
-      desiredChainId={9789.Testnet}
+      chainRpc={{ [Chainid.Testnet]: " https://rpc.testnet.tabichain.com" }}
+      desiredChainId={Chainid.Testnet}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
       <Navbar />
