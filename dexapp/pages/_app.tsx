d961f643a86512e971250e25ad3a0217e93070e5
Mallu Crypto <161009845+RealMallu@users.noreply.github.com>
2024-03-29 23:53:12 +0530

    Update _app.tsx

--- dexapp/pages/_app.tsx
+++ dexapp/pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       chainRpc={{ [9789.Testnet]: " https://rpc.testnet.tabichain.com" }}
-      desiredChainId={C9789.Testnet}
+      desiredChainId={9789.Testnet}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
       <Navbar />
